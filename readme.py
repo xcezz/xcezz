8ac72f7ab3e94e66b14efb0f9c36f29d896c8a4b
xcezz <me@xcezz.de>
2026-08-16 19:36:57 +0200

    Remove `ORGANIZATION_MEMBER` from log_query's scope

--- readme.py
+++ readme.py
@@ -456,7 +456,7 @@ if __name__ == '__main__':
     formatter('account data', user_time)
     age_data, age_time = perf_counter(daily_readme, datetime.datetime(1992, 3, 28))
     formatter('age calculation', age_time)
-    total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'], 7)
+    total_loc, loc_time = perf_counter(loc_query, ['OWNER', 'COLLABORATOR'], 7)
     formatter('LOC (cached)', loc_time) if total_loc[-1] else formatter('LOC (no cache)', loc_time)
     commit_data, commit_time = perf_counter(commit_counter, 7)
     (repo_data, star_data), repo_star_time = perf_counter(graph_repos_stars, 'both', ['OWNER']) # stars and repos come from the same response
